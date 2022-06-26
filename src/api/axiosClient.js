2948092be503973e9aa14a1132135f99bb8f26a5
Devnaplandau <Devnaplandau@gmail.com>
2022-06-26 13:34:57 +0700

    100%

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import queryString from "query-string";
 
-const baseUrl = "http://127.0.0.1:3000/api/";
+const baseUrl = "http://covid-tdu-server.herokuapp.com/api/";
 const getToken = () => localStorage.getItem("token");
 
 const axiosClient = axios.create({
